91ee376532ac18d00628cd227be35416fb2915a8
Greg Hale <imalsogreg@gmail.com>
2015-06-06 10:39:27 -0400

    whack-a-mole logic working

--- src/Main.hs
+++ src/Main.hs
@@ -17,7 +17,6 @@ import           Data.Monoid ((<>))
 --import Text.Blaze.Html (toHtml)
 --import Text.Blaze.Html5 as H
 --import Text.Blaze.Html5.Attributes as A hiding (value)
-import           Codec.Picture
 import           Data.Time
 import           Data.Time.Clock
 import           Data.Default
@@ -55,14 +54,13 @@ main = do
   runWebGUI $ \webView -> do
     doc <- waitUntilJust $ liftM (fmap castToHTMLDocument) $
            webViewGetDomDocument webView
-    let btag = "reflex-area"
+    let btag = "reflex-area" :: String
     root <- waitUntilJust $ liftM (fmap castToHTMLElement) $
             documentGetElementById doc btag
-    attachWidget root webView runApp
+    attachWidget root webView (runApp tStart rnd)
 
-runApp :: (forall t m. MonadWidget t m, RandomGen g) => UTCTime -> g -> m ()
+runApp :: forall t m g.(MonadWidget t m, RandomGen g) => UTCTime -> g -> m ()
 runApp tStart rnd = do
-  mainWidgetWithCss $(embedFile "css/default.css") $ do
     elClass "div" "content" $ do
       demoWidget "Echo textbox contents to a div below"
         textEchoCode textEcho
@@ -76,6 +74,8 @@ runApp tStart rnd = do
         inhomPoissonDemoCode (inhomPoissonDemo rnd tStart)
       demoWidget "Bouncing Ball"
         "in progress" (demoBounce tStart)
+      demoWidget "Whack-a-Mole"
+        "in progress" (whackAMole rnd tStart)
 
 demoWidget :: MonadWidget t m => String -> String -> m a -> m a
 demoWidget descr src w = do
@@ -253,7 +253,11 @@ demoBounce = text "Is it possible?" & return
 
 whackAMole :: (RandomGen g, MonadWidget t m) => g -> UTCTime -> m ()
 whackAMole rnd t0 = mdo
-  undefined
+  let (r,r') = split rnd
+  moleWidget r t0 (constDyn 0.5)
+  el "br" (return ())
+  moleWidget r t0 (constDyn 0.2)
+  return ()
 
 -- moleWidget handles one active mole, triggering internal pop-up events and returning
 -- events that signal a successfull whack or unsuccessful appear-disappear sequences
@@ -261,36 +265,36 @@ moleWidget :: (RandomGen g, MonadWidget t m)
            => g
            -> UTCTime
            -> Dynamic t Double
-           -> m (Event t)
+           -> m (Event t MoleState)
 moleWidget rnd t0 popupRate = mdo
 
-  picAttrs    <- forDyn moleState $ \s -> mappend (sAttr s) baseAttrs
-    where sAtters s = case s of
-                       MoleUp      -> "src" := "MoleUp.png"
-                       MoleDown    -> "src" := "MoleDown.png"
-                       MoleWhacked -> "src" := "MoleWhacked.png"
-          baseAttrs = ["class" := "mole-pic"]
+  --picAttrs    <- forDyn moleState $ \s -> mappend (pSrc s) ["class" =: "mole-pic"]
+  let pSrc s  = case s of
+                 MoleUp      -> "src" =: "MoleUp.png"
+                 MoleDown    -> "src" =: "MoleDown.png"
+                 MoleWhacked -> "src" =: "MoleWhacked.png"
   --molePic     <- elDynAttr "img" picAttrs
-  molePic     <- forDyn moleState show >>= elDynHtml "div"
-  (r,r')      =  split rnd
+  molePic     <- forDyn moleState show >>= elDynHtml' "div"
+  let (r,r')  =  split rnd
 
-  popupTimes  <- inhomogeneousPoisson r popupRate 5 t0
+  popupTimes  <- inhomogeneousPoisson r (current popupRate) 5 t0
   goDownTimes <- mapDyn (*2) popupRate >>= \goDownRate ->
-                 inhomogeneousPoisson r' goDownRate 10 t0
+                 inhomogeneousPoisson r' (current goDownRate) 10 t0
   let whacks  =  _el_clicked molePic
-  moleState   <- foldDyn (\s e -> updateMole s e) Down
+  moleState   <- foldDyn (\e s -> updateMole e s) MoleDown
                  (leftmost [fmap (const GoUp)   popupTimes
                            ,fmap (const GoDown) goDownTimes
                            ,fmap (const Whack)  whacks
                            ])
+  return . ffilter (== MoleWhacked) . updated $ moleState
 
 
-updateMole :: MoleState -> MoleAction -> MoleState
-updateMole MoleDown    GoUp   = MoleUp
-updateMole MoleUp      GoDown = MoleDown
-updateMole MoleUp      Whack  = MoleDown
-updateMole MoleWhacked GoDown = MoleDown
-updateMole s           _      = s
+updateMole :: MoleAction -> MoleState -> MoleState
+updateMole GoUp   MoleDown    = MoleUp
+updateMole GoDown MoleUp      = MoleDown
+updateMole Whack  MoleUp      = MoleWhacked
+updateMole GoDown MoleWhacked = MoleDown
+updateMole _ s                 = s
 
 data MoleState = MoleDown | MoleUp | MoleWhacked
                deriving (Eq, Show)
